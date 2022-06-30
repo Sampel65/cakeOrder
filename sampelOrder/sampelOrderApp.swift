84424f54ebaabb11dc50b8ea50c85f9bebbc7cb4
Sampel65 <sampeldan65@gmail.com>
2022-06-30 16:29:21 +0100

    Initial Commit

---
+++ sampelOrder/sampelOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampelOrderApp.swift
+//  sampelOrder
+//
+//  Created by Sampel on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct sampelOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
